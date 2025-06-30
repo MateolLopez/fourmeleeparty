14fe6bdf74f54d759c41be9554b9e8089c8a6826
MateolLopez <j.mateolopez.94@gmail.com>
2025-06-30 15:39:58 -0300

    a

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 import 'zone.js';
 
 bootstrapApplication(App, {
   providers: [
     provideHttpClient(withFetch()),
-    provideRouter(routes)
+    provideRouter(routes, withHashLocation()) 
   ]
 });
